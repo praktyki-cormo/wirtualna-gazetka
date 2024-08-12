8ffd8a9ccfe441c044dd22395d1ec757d20e3403
richard <justrichardtbh@proton.me>
2024-08-12 09:28:23 +0200

    Move header styles to css

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {Link} from 'gatsby'
 import logo from '../images/wirtualnagazetka.svg'
 import "./header.css"
+import { listeners } from 'process'
 
 const Header = () => {
     const handleNewspaperClick = () => {
@@ -12,44 +13,6 @@ const Header = () => {
     }
     const handleLoginClick = () => {
         //logowanie
-    }
-
-    var headerStyle = {
-        color: "white",
-        backgroundColor: "#246036",
-        textAlign: "center",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: 'space-between',
-        marginTop: 10,
-        marginBottom: 0,
-        lineHeight: 1.25,
-        padding: 10,
-        margin: 0,
-        width: '100%',
-        boxShadow: "-2px 2px 10px 2px rgba(0, 0, 0, 0.25)",
-        boxSizing: 'border-box'
-    }
-    var headerPosition = {
-        position: 'sticky',
-        top: 0,
-        left: 0,
-        transition: 'top 0.25s ease-out',
-        zIndex: 40
-    }
-    var headerElemStyle = {
-        margin: 10,
-        maxWidth: 80,
-        padding: '0px 20px 0px 20px',
-        fontSize: 14,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        color: 'white',
-        fontWeight: 'bold'
-    }
-    var headerElemContainerStyle = {
-        display: 'flex'
     }
 
     const [y,
@@ -75,52 +38,47 @@ const Header = () => {
           window.removeEventListener("scroll", handleNavigation);
         };
       }, [handleNavigation]);
-    if (window.scrollY > document.getElementById("header")?.offsetHeight) {
-        scrollDirection ? headerPosition.top = 0:headerPosition.top = -150
-    }
+      
+    //document.getElementById("header")?.addEventListener(() => {})
 
-    if (window.scrollY < document.getElementById("header")?.offsetHeight) {
-        headerPosition.transition = 'top 0.0s ease-out'
-    } else {
-        headerPosition.transition = 'top 0.25s ease-out'
-    }
+    // if (window.scrollY > document.getElementById("header")?.offsetHeight) {
+    //     scrollDirection ? headerPosition.top = 0:headerPosition.top = -150
+    // }
 
     return(
-        <header style={headerPosition} id="header">
-            <div style={headerStyle}>
-                <div>
-                    <img src={logo} style={{
-                        height: 50,
-                        flexGrow: 3
-                    }}/>
-                </div>
+        <header className="headerPosition" id="header">
+            <div className="headerStyle">
                 <div style={{
                     display: 'flex',
                     paddingRight: 20,
                     textAlign: 'center'
                 }}>
-                    <div style={headerElemContainerStyle}>
-                        <Link to='/kontakt' style={{...headerElemStyle, textDecoration: 'none'}} className='button'>
+                    <img src={logo} style={{
+                        height: 50,
+                        flexGrow: 3
+                    }}/>
+                    <div className='headerElemContainerStyle'>
+                        <Link to='/kontakt' style={{textDecoration: 'none'}} className='button'>
                         <p >Kontakt</p>
                         </Link>
                     </div>
-                    <div style={headerElemContainerStyle}>
-                        <p style={headerElemStyle} className='button' onClick={handleNewspaperClick}>
+                    <div className='headerElemContainerStyle'>
+                        <p className='button' onClick={handleNewspaperClick}>
                             Zamów
                         </p>
                     </div>
-                    <div style={headerElemContainerStyle}>
-                        <p style={headerElemStyle} className='button' onClick={handlePricingClick}>
+                    <div className='headerElemContainerStyle'>
+                        <p className='button' onClick={handlePricingClick}>
                             Wycena
                         </p>
                     </div>
-                    <div style={headerElemContainerStyle}>
-                        <Link to='/o-nas' style={{...headerElemStyle, textDecoration: 'none'}} className='button'>
+                    <div className='headerElemContainerStyle'>
+                        <Link to='/o-nas' style={{textDecoration: 'none'}} className='button'>
                         <p >O nas</p>
                         </Link>
                     </div>
-                    <div style={headerElemContainerStyle}>
-                        <p style={headerElemStyle} className='panel' onClick={handleLoginClick}>
+                    <div className='headerElemContainerStyle'>
+                        <p className='panel' onClick={handleLoginClick}>
                             Panel
                         </p>
                     </div>
